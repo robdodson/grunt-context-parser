f297ad47e1a4e82add864cb8f61b06504f324e5d
Rob Dodson <robdodson@google.com>
2015-01-18 10:31:11 -0800

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+/*
+ * grunt-context-parser
+ *
+ *
+ * Copyright (c) 2015
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  // load all npm grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['test/tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    context_parser: {
+      default: {
+        options: {
+          merge: true
+        },
+        src: 'test/fixtures',
+        dest: 'test/tmp'
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  // grunt.registerTask('test', ['clean', 'context_parser', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'context_parser']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/context_parser.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.registerMultiTask('context_parser', 'Run elements through context-free-parser', function () {
+    var generate = require('context-parser');
+    var options = this.options({
+      merge: false
+    });
+
+    grunt.file.mkdir(this.data.dest);
+    generate(this.data.src, this.data.dest, options);
+  });
+};
